f6289a271007c08d1f5b764d384c07a97ba6ab53
Stefan Schultz <28778796+itsmestefanjay@users.noreply.github.com>
2021-03-05 14:06:32 +0100

    remove JavaDelegate implementation
    we use expression in the process model instead

--- flowable/src/main/java/de/novatec/bpm/delegate/LoggerDelegate.java
+++ flowable/src/main/java/de/novatec/bpm/delegate/LoggerDelegate.java
@@ -1,16 +1,13 @@
 package de.novatec.bpm.delegate;
 
-import org.flowable.engine.delegate.DelegateExecution;
-import org.flowable.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LoggerDelegate implements JavaDelegate {
+public class LoggerDelegate {
 
     private final Logger logger = LoggerFactory.getLogger(LoggerDelegate.class);
 
-    @Override
-    public void execute(DelegateExecution execution) {
+    public void log(String message) {
         logger.info("Hello, Flowable!");
     }
 }
