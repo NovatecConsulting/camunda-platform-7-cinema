3e6459abdbb289836956f731a37f34d6d7f26179
itsmestefanjay <28778796+itsmestefanjay@users.noreply.github.com>
2021-03-19 14:00:25 +0100

    add payment error flag to force compensation

--- camunda/src/main/java/de/novatec/bpm/config/ServiceConfig.java
+++ camunda/src/main/java/de/novatec/bpm/config/ServiceConfig.java
@@ -1,15 +1,19 @@
 package de.novatec.bpm.config;
 
 import de.novatec.bpm.service.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServiceConfig {
 
+    @Value("${cinema.delegate.payment.error:false}")
+    public boolean throwErrors;
+
     @Bean
     public PaymentService paymentService() {
-        return new PaymentService();
+        return new PaymentService(throwErrors);
     }
 
     @Bean
--- services/src/main/java/de/novatec/bpm/service/PaymentService.java
+++ services/src/main/java/de/novatec/bpm/service/PaymentService.java
@@ -1,19 +1,30 @@
 package de.novatec.bpm.service;
 
 import de.novatec.bpm.exception.PaymentException;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PaymentService {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final boolean throwErrors;
+
+    public PaymentService(boolean throwErrors) {
+        this.throwErrors = throwErrors;
+    }
 
     public void issueMoney(int ticketPrice, String iban, String bic) {
-        if (Math.random() < 0.25) {
-            throw new PaymentException("There was an issue with the payment");
+        if (throwErrors && Math.random() > 0.5) {
+            logger.error("There was an issue with the payment");
+            throw new PaymentException("Bank declined the transaction. Code: " + generateRandomBankReference());
         } else {
             logger.info("Getting {} Euro from IBAN {}, BIC {}", ticketPrice, iban, bic);
         }
+    }
+
+    private String generateRandomBankReference() {
+        return RandomStringUtils.random(12, true, true).toUpperCase();
     }
 
     public void giveMoneyBack(int ticketPrice, String iban, String bic) {
